fb50ae68cb9317be293e091d63950ca2ea81d420
Semaj Thomas-Primm <semajthomasprimm@users.noreply.github.com>
2017-06-25 13:22:06 -0400

    Fix Contact Form #1
    Deleted ajax code.

--- contact-form.js
+++ contact-form.js
@@ -1,16 +1,1 @@
-$('form#contactForm').validate({
-  messages: { },
-  submitHandler: function(form) {
-    $.ajax({
-      url: "//formspree.io/ajtames@gmail.com",
-      method: "POST",
-      data: $(form).serialize(),
-      dataType: "json",
-      success: function(data) {
-          $("form#contactForm :input").prop("disabled", true);
-          $('#thanks').show();
-      }
-    });
-    return false;
-  }
-});
+
